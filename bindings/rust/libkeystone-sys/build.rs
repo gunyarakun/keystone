2aeb2d2b60907d8bbbdec68dd7a0400ffff894d4
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2017-10-27 23:28:18 -0700

    Add features

--- bindings/rust/libkeystone-sys/build.rs
+++ bindings/rust/libkeystone-sys/build.rs
@@ -1,40 +1,50 @@
-extern crate os_type;
-extern crate build_helper;
+#[cfg(feature = "use_system_keystone")]
+extern crate pkg_config;
 
+use std::fs;
 use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 
-fn main() {
+fn build_with_cmake() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let build_dir = out_dir.join("build");
+    let cmake_dir = fs::canonicalize("../../..").unwrap();
 
-    run(
-        Command::new("mkdir")
-            .current_dir("../../..")
-            .arg("-p")
-            .arg("build"),
-    );
+    run(Command::new("mkdir").current_dir(&out_dir).arg("-p").arg(
+        "build",
+    ));
 
-    run(Command::new("cmake").current_dir("../../../build").args(
+    run(Command::new("cmake").current_dir(&build_dir).args(
         &[
             &format!("-DCMAKE_INSTALL_PREFIX={}", out_dir.display()),
             "-DCMAKE_BUILD_TYPE=Release",
             "-DBUILD_LIBS_ONLY=1",
-            "-DCMAKE_SHARED_LIBS=OFF",
             "-DCMAKE_OSX_ARCHITECTURES=",
+            "-DBUILD_SHARED_LIBS=ON",
             "-DLLVM_TARGET_ARCH=host",
             "-G",
             "Unix Makefiles",
-            "..",
+            cmake_dir.to_str().unwrap(),
         ],
     ));
 
-    run(Command::new("make").current_dir("../../../build").arg(
-        "install",
-    ));
+    run(Command::new("make").current_dir(&build_dir).arg("install"));
 
     println!("cargo:rustc-link-search=native={}/lib", out_dir.display());
     println!("cargo:rustc-link-lib=keystone");
+}
+
+fn main() {
+    if cfg!(feature = "use_system_keystone") {
+        #[cfg(feature = "use_system_keystone")]
+        {
+            pkg_config::find_library("keystone").expect("Could not find system keystone");
+            println("cargo:rustc-link-lib=dylib=keystone");
+        }
+    } else {
+        build_with_cmake();
+    }
 }
 
 fn run(cmd: &mut Command) {
